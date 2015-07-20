60d04dbdb704535584f692c20966b14195fb28ab
cclauss <cclauss@bluewin.ch>
2015-07-20 16:22:29 +0200

    Added read_floats() and write_floats() functions

--- binary_file_of_2d_matrix.py
+++ binary_file_of_2d_matrix.py
@@ -6,6 +6,9 @@ import array, os, struct
 
 my_matrix_2d = [(x, x * 10) for x in xrange(10)]
 
+def flatten(in_list):
+    return [item for sublist in in_list for item in sublist]
+
 def list_of_pairs(in_list):
     out_list = []
     while in_list:
@@ -15,36 +18,43 @@ def list_of_pairs(in_list):
     return out_list
 
 # === do it with the array module
-def write_2d_matrix_via_array(matrix_2d, filename='matrix_via_array.txt'):
-    def flatten(in_list):
-        return [item for sublist in in_list for item in sublist]
-    a = array.array('f', flatten(matrix_2d))
+def write_floats_via_array(floats, filename='matrix_via_array.txt'):
     with open(filename, 'wb') as out_file:
-        a.tofile(out_file)
-    
-def read_2d_matrix_via_array(filename='matrix_via_array.txt'):
+        array.array('f', floats).tofile(out_file)
+
+def read_floats_via_array(filename='matrix_via_array.txt'):
     floats_in_the_file = os.path.getsize(filename) / struct.calcsize('f')
     b = array.array('f')
     with open(filename, 'rb') as in_file:
         b.fromfile(in_file, floats_in_the_file)
-    return list_of_pairs(b.tolist())
+    return b.tolist()
+
+def write_2d_matrix_via_array(matrix_2d, filename='matrix_via_array.txt'):
+    write_floats_via_array(flatten(matrix_2d), filename)
     
-print('=' * 20)
+def read_2d_matrix_via_array(filename='matrix_via_array.txt'):
+    return list_of_pairs(read_floats_via_array(filename))
+
+print('=' * 24)
 write_2d_matrix_via_array(my_matrix_2d)
 print(read_2d_matrix_via_array())
 
 # === do it with the struct module
-def read_2d_matrix_via_struct(filename='matrix_via_struct.txt'):
+def read_floats_via_struct(filename='matrix_via_struct.txt'):
     with open(filename, 'rb') as in_file:
         data = in_file.read()
-    fmt = len(data) / struct.calcsize('f') * 'f'
-    return list_of_pairs(struct.unpack_from(fmt, data))
+    return struct.unpack_from(len(data) / struct.calcsize('f') * 'f', data)
+
+def write_floats_via_struct(floats, filename='matrix_via_struct.txt'):
+    with open(filename, 'wb') as out_file:
+        out_file.write(struct.pack('f' * len(floats), *floats))
 
 def write_2d_matrix_via_struct(matrix_2d, filename='matrix_via_struct.txt'):
-    with open(filename, 'wb') as out_file:
-        for x, y in matrix_2d:
-            out_file.write(struct.pack('ff', x, y))
+    write_floats_via_struct(flatten(matrix_2d), filename)
+    
+def read_2d_matrix_via_struct(filename='matrix_via_struct.txt'):
+    return list_of_pairs(read_floats_via_struct(filename))
 
-print('=' * 10)
+print('=' * 12)
 write_2d_matrix_via_struct(my_matrix_2d)
 print(read_2d_matrix_via_struct())
