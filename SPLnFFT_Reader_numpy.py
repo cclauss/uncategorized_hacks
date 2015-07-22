6df63fbed36e1167a2de3babc0e14b455edb59a8
cclauss <cclauss@bluewin.ch>
2015-07-22 17:50:46 +0200

    Update SPLnFFT_Reader_numpy.py

--- SPLnFFT_Reader_numpy.py
+++ SPLnFFT_Reader_numpy.py
@@ -5,11 +5,11 @@
 
 # Now I understand why numpy is all the rage with data scientists!!!
 
-# Three lines of numpy do the while thing!! Import, read, transform,
-# and cleanse.  Much faster execution time too!
+# Three lines of numpy do the whole thing!! Import, read, transform,
+#  and cleanse.  Much faster execution time too.
 
 import numpy
 data = numpy.fromfile('SPLnFFT_2015_07_21.bin', dtype=numpy.float32).reshape(-1, 2)
-data = data[numpy.all(data > 0, axis=1)]
+data = data[numpy.all(data > 0, axis=1)]  # cleanse
 print(type(data), len(data))  # numpy.ndarray, 2786
-print(data[:20])  # print the first 20 fast_slow pairs
+print(data[:20])  # print first 20 fast, slow pairs
