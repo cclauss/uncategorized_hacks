52295a2bc91ec03e0dd8ecf8b686e04d4c8cdffe
cclauss <cclauss@bluewin.ch>
2015-07-22 17:43:55 +0200

    Update SPLnFFT_Reader_numpy.py

--- SPLnFFT_Reader_numpy.py
+++ SPLnFFT_Reader_numpy.py
@@ -5,12 +5,11 @@
 
 # Now I understand why numpy is all the rage with data scientists!!!
 
-# Four lines of numpy do the whole thing!! Import, read, transform, and cleanse.
+# Three lines of numpy do the while thing!! Import, read, transform, and cleanse.
 # Much faster execution time too.
 
 import numpy
-data = numpy.fromfile('SPLnFFT_2015_07_21.bin', dtype=numpy.float32)
-data.shape = (len(data) / 2, 2)
+data = numpy.fromfile('SPLnFFT_2015_07_21.bin', dtype=numpy.float32).reshape(-1, 2)
 data = data[numpy.all(data > 0, axis=1)]
 print(type(data), len(data))  # numpy.ndarray, 2786
 print(data[:20])
