b708ddad72c568b94ce69d1add91c03ae56af113
cclauss <cclauss@bluewin.ch>
2016-06-06 09:21:20 +0200

    flake8 recommended changes

--- pypi_top200.py
+++ pypi_top200.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python3
 
-MAX_PACKAGES = 200  # or try 1000
-USE_ASYNCIO = True
-
 import asyncio
 import collections
 import time
@@ -11,11 +8,15 @@ try:
 except ImportError:
     from xmlrpclib import ServerProxy
 
+MAX_PACKAGES = 200  # or try 1000
+PYPI_URL = 'https://pypi.python.org/pypi'
+USE_ASYNCIO = True
+
 pkg_info = collections.namedtuple('pkg_info', 'pkg_name downloads version py3')
-fmt = '{pkg_name:30}{version:13}{py3}'
+FMT = '{pkg_name:30}{version:13}{py3}'
 py3_classifier = 'Programming Language :: Python :: 3'
 
-client = ServerProxy('https://pypi.python.org/pypi')
+client = ServerProxy(PYPI_URL)
 
 
 def header():
@@ -26,7 +27,7 @@ def header():
 
 def get_pkg_info(pkg_name, downloads=0):
     # multiple asyncio jobs can not share a client
-    local_client = ServerProxy('https://pypi.python.org/pypi') if USE_ASYNCIO else client
+    local_client = ServerProxy(PYPI_URL) if USE_ASYNCIO else client
     try:
         release = local_client.package_releases(pkg_name)[0]
     except IndexError:  # marionette-transport, ll-orasql, and similar
@@ -47,31 +48,32 @@ def async_main():  # ~ 32 seconds for 200 packages on my MacBook Pro
 
 def sync_main():
     # as list for apples-to-apples benchmark - ~ 112 seconds
-#   return [get_pkg_info(pkg_name, downloads) for pkg_name, downloads
-#           in client.top_packages(MAX_PACKAGES)]
+    # return [get_pkg_info(pkg_name, downloads) for pkg_name, downloads
+    #         in client.top_packages(MAX_PACKAGES)]
 
     # or as a generator for continuous output - ~ 105 seconds
     return (get_pkg_info(pkg_name, downloads) for pkg_name, downloads
             in client.top_packages(MAX_PACKAGES))
 
 
-print('Gathering Python 3 support info on the top {} PyPI packages...'.format(MAX_PACKAGES))
+fmt = 'Gathering Python 3 support info on the top {} PyPI packages...'
+print(fmt.format(MAX_PACKAGES))
 start = time.time()
 if USE_ASYNCIO:
     loop = asyncio.get_event_loop()
     packages_info = loop.run_until_complete(async_main())
 else:
     packages_info = sync_main()
-print(time.time() - start, 'seconds')  # ~ 32 if USE_ASYNCIO else ~ 112
+print(time.time() - start, 'seconds')  # ~ 32 sec if USE_ASYNCIO else ~ 105 sec
 
 print(header())
 for package_info in packages_info:
-    print(fmt.format(**package_info._asdict()))
+    print(FMT.format(**package_info._asdict()))
 
 losers = [pkg for pkg in packages_info if not pkg.py3]
 print('\n{} Python 2 ONLY packages:'.format(len(losers)))
 if losers:
     print(header())
-    print('\n'.join(fmt.format(**pkg._asdict()) for pkg in losers))
+    print('\n'.join(FMT.format(**pkg._asdict()) for pkg in losers))
 
 print(time.time() - start, 'seconds')
